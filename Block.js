8093dca457bd126d71e17a7429fc35e69471038d
AniketSharmap <73336096+AniketSharmap@users.noreply.github.com>
2020-12-23 10:58:38 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,44 @@
+class Block 
+{
+    constructor(x, y, width, height) 
+    {
+        var options =
+        {
+            isStatic: false,
+            resitution : 0.8,
+            friction:1.0, 
+            density:1.0, 
+        }
+        this.visibility = 255;
+        this.body = Bodies.rectangle(x, y, width, height, options)
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        World.add(world,this.body);
+    }
+    display()
+    {
+        // console.log(this.body.speed);
+        if(this.body.speed < 3)
+        {
+            var pos = this.body.position;
+            var angle = this.body.angle;
+            push();
+            translate(pos.x,pos.y);
+            rotate(angle);
+            fill("blue");
+            rectMode(CENTER);
+            rect(0,0,this.width,this.height);
+            pop();
+        }
+        else
+        {
+            World.remove(world,this.body);
+            push();
+            this.visibility = this.visibility - 5;
+            tint(255,this.visibility)
+            pop();
+        }
+    }
+};
